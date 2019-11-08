5721170d88b28e84ceb97193f87bcb68964ab8f8
Sean Grady <vedicmonk@gmail.com>
2019-11-08 15:01:54 -0800

    added function to turn an integer into the reversed linked list that the problem wants

--- add_two_numbers/add_numbers.py
+++ add_two_numbers/add_numbers.py
@@ -1,9 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
+    def create_linked_list(self, number: int) -> ListNode:
+        number_list = [int(char) for char in str(number)]
+        number_list.reverse()
+        head = ListNode(number_list[0])
+        prev_node = head
+        for value in number_list[1:]:
+            node = ListNode(value)
+            prev_node.next = node
+            prev_node = node
+        return head
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        if l1.val == 0:
+            return l2
+        if l2.val == 0:
+            return l1
+        
