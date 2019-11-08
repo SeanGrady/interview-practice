50ba87b611b9a6d674173b96c5870df1f67605df
Sean Grady <vedicmonk@gmail.com>
2019-11-08 15:02:17 -0800

    Added single shitty test for the int-to-reversed-linked-list function

--- add_two_numbers/tests.py
+++ add_two_numbers/tests.py
@@ -2,7 +2,24 @@ import unittest
 import add_numbers
 
 
+class TestMakeLL(unittest.TestCase):
+    def test_make_long_number(self):
+        number = 12345
+        list_creator = add_numbers.Solution().create_linked_list
+        head_node = list_creator(number)
+        self.assertEqual(head_node.val, 5)
+        self.assertEqual(head_node.next.val, 4)
+        self.assertEqual(head_node.next.next.val, 3)
+        self.assertEqual(head_node.next.next.next.val, 2)
+        self.assertEqual(head_node.next.next.next.next.val, 1)
+        self.assertEqual(head_node.next.next.next.next.next, None)
+
+
 class TestAddNumbers(unittest.TestCase):
 
     def test_two_zeros(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
