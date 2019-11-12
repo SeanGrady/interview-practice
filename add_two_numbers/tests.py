865f4b98d5aec2c2a88f196341a9817591209cee
Sean Grady <vedicmonk@gmail.com>
2019-11-12 15:58:07 -0800

    added more test cases, refactor existing tests to take only an ingeter and then use existing int->ll and ll->int functions to do all the testing from there

--- add_two_numbers/tests.py
+++ add_two_numbers/tests.py
@@ -3,10 +3,15 @@ import add_numbers
 
 
 class TestMakeLL(unittest.TestCase):
+    def setUp(self):
+        self.solution = add_numbers.Solution()
+        self.list_creator = self.solution.create_linked_list
+        self.number_creator = self.solution.linked_list_to_int
+
+
     def test_make_long_number(self):
         number = 12345
-        list_creator = add_numbers.Solution().create_linked_list
-        head_node = list_creator(number)
+        head_node = self.list_creator(number)
         self.assertEqual(head_node.val, 5)
         self.assertEqual(head_node.next.val, 4)
         self.assertEqual(head_node.next.next.val, 3)
@@ -14,20 +19,44 @@ class TestMakeLL(unittest.TestCase):
         self.assertEqual(head_node.next.next.next.next.val, 1)
         self.assertEqual(head_node.next.next.next.next.next, None)
 
+    def test_list_to_int(self):
+        number = 2318473
+        number_list = self.list_creator(number)
+        recovered_number = self.number_creator(number_list)
+        self.assertEqual(number, recovered_number)
+
+    def test_zero_both_directions(self):
+        number = 0
+        number_list = self.list_creator(number)
+        recovered_number = self.number_creator(number_list)
+        self.assertEqual(number, recovered_number)
 
 class TestAddNumbers(unittest.TestCase):
     def setUp(self):
-        solution = add_numbers.Solution()
-        self.list_creator = solution.create_linked_list
-        self.adder = solution.addTwoNumbers
+        self.solution = add_numbers.Solution()
+        self.list_creator = self.solution.create_linked_list
+        self.adder = self.solution.addTwoNumbers
+        self.number_creator = self.solution.linked_list_to_int
 
     def test_two_zeros(self):
         int_1 = 0
         int_2 = 0
+        answer = 0
         l1_head = self.list_creator(int_1)
         l2_head = self.list_creator(int_2)
         sum_head = self.adder(l1_head, l2_head)
-        self.assertEqual(0, sum_head.val)
+        sum_int = self.number_creator(sum_head)
+        self.assertEqual(answer, sum_int)
+
+    def test_single_digits(self):
+        int_1 = 5
+        int_2 = 3
+        answer = 8
+        head_1 = self.list_creator(int_1)
+        head_2 = self.list_creator(int_2)
+        sum_head = self.adder(head_1, head_2)
+        sum_int = self.number_creator(sum_head)
+        self.assertEqual(answer, sum_int)
 
 
 if __name__ == '__main__':
